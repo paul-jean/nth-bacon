c596481dd3205e49732fd6c0533b9156d13e811c
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-06-03 14:53:52 -0500

    Add tests.

--- tests/test-shortest-paths.js
+++ tests/test-shortest-paths.js
@@ -1,4 +1,24 @@
 var ShortestPaths = require('../ShortestPaths.js');
+var assert = require('assert');
+var util = require('util');
+
+var testPath = function(graph, source, dest, expectedPathIDs) {
+  console.log('Test: actor graph =');
+  console.log(graph);
+
+  // build shortest paths to source node:
+  var shortestPaths = new ShortestPaths(graph, source);
+  // retrieve the shortest path from destination node:
+  var pathTo1 = shortestPaths.pathTo(dest);
+  var ids = [];
+  pathTo1.forEach(function(e) { ids.push(e.id); });
+  assert.deepEqual(ids, expectedPathIDs, 'Test FAILED: path returned was ' + ids);
+
+  console.log('Test SUCCEEDED.');
+  console.log(util.format('Shortest path from %d to %d =', dest, source));
+  shortestPaths.printPathTo(dest);
+  console.log('\n++++++++++++++++++++++++++++++++++++++\n');
+};
 
 /*
  * 1 --- (1-2) --- 2
@@ -15,7 +35,17 @@ var actorGraph = {
   3: {1: '3-1', 2: '3-2'}
 };
 
-// build shortest paths to node 3:
-var shortestPaths = new ShortestPaths(actorGraph, 3);
-// retrieve the shortest path from node 1:
-shortestPaths.printPathTo(1);
+testPath(actorGraph, 3, 1, [1, 3]);
+
+// example graph from pp 537 of
+// "Algorithms", Sedgewick et al
+actorGraph2 = {
+  0: {2: '0-2', 1: '0-1', 5: '0-5'},
+  1: {0: '1-0', 2: '1-2'},
+  2: {0: '2-0', 1: '2-1', 3: '2-3', 4: '2-4'},
+  3: {5: '3-5', 4: '3-4', 2: '3-2'},
+  4: {3: '4-3', 2: '4-2'},
+  5: {3: '5-3', 0: '5-0'}
+};
+
+testPath(actorGraph2, 0, 3, [3, 2, 0]);
