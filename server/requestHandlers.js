a0efce231f445b2fe9e5545efc83a6eb952d7134
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-06-27 01:27:26 -0400

    Get AJAX call to /findPath working.

--- server/requestHandlers.js
+++ server/requestHandlers.js
@@ -24,7 +24,10 @@ var findPath = function(response, postData) {
   console.log("Handling findPath.");
   // TODO: find the shortest bacon path in the actor graph
   var body = postData; // echo what what sent from the client
-  response.writeHead(200, {"Content-type": "text/html"});
+  response.writeHead(200, {
+    "Content-type": "text/html",
+    "Access-Control-Allow-Origin": "*"
+  });
   response.write(pathString);
   response.end();
 };
